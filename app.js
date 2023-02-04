21d6cc662e579835937b922c73de92046b082f2d
derkky <don_foh@hotmail.com>
2023-02-04 15:01:36 +0800

    added back helmet and cors

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.use(helmet())
+app.use(cors())
 
 app.use("/api/posts", postRoutes)
 app.use("/api/comments", commentRoutes)
